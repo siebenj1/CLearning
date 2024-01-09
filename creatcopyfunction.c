74b64193e376b9a1d3280a2b107509459349187f
siebenj1 <85626649+siebenj1@users.noreply.github.com>
2024-01-09 14:11:59 -0600

    Create creatcopyfunction.c

---
+++ creatcopyfunction.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+ 
+void copy(char* dst, char* src){
+  while (*src != '\0'){ //tells while loop to continue as long as it does not meed the terminating character
+    *dst = *src;
+    //intial made a mistake with *src = *dst
+    src++;
+    dst++;
+  }
+  *dst = '\0';
+}
+ 
+int main(){
+  char srcString[] = "We promptly judged antique ivory buckles for the next prize!";
+    char i = strlen(srcString) + 1;
+    char dstString[i];
+    copy(dstString, srcString);
+    printf("%s", dstString);
+ 
+}
