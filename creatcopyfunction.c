81f246b1e2e58dea48bd653f84fcf2d48b4ec092
siebenj1 <85626649+siebenj1@users.noreply.github.com>
2024-01-09 14:12:20 -0600

    Update creatcopyfunction.c

--- creatcopyfunction.c
+++ creatcopyfunction.c
@@ -2,7 +2,7 @@
 #include<string.h>
  
 void copy(char* dst, char* src){
-  while (*src != '\0'){ //tells while loop to continue as long as it does not meed the terminating character
+  while (*src != '\0'){ //tells while loop to continue as long as it does not meet the terminating character of the string
     *dst = *src;
     //intial made a mistake with *src = *dst
     src++;
